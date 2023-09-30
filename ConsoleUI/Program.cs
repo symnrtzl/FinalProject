95825f80b5eae04eb839459cebb0733d6668ad41
symnrtzl <symnrtzl99@gmail.com>
2023-09-30 19:28:33 +0300

    stage-three

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,27 +9,62 @@ namespace ConsoleUI
     //Open Closed Principle -> Yaptığın yazılıma yeni bir özellik ekliyorsan mevcuttaki hiç bir koda dokunamazsın.
     class Program
     {
-        static void Main(string[] args) 
+        static void Main(string[] args)
+        {
+            //DTO :Data Transformation Object
+            //ProductTest();
+            //CategoryTest();
+
+            ProductDetailsDtoTest();
+
+
+
+
+        }
+
+
+
+
+
+
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            /*
-             foreach (var product in productManager.GetAll())
+
+            foreach (var product in productManager.GetAll())
             {
                 Console.WriteLine(product.ProductName);
             }
-             */
-
-            /*
             foreach (var product in productManager.GetAllByCategoryId(2))
             {
                 Console.WriteLine(product.ProductName);
             }
-            */
-
-            foreach (var product in productManager.GetByUnitPrice(50,100))
+            foreach (var product in productManager.GetByUnitPrice(50, 100))
             {
                 Console.WriteLine(product.ProductName);
             }
         }
+
+        private static void ProductDetailsDtoTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetProductDetails())
+            {
+                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+            }
+        }
+
+
     }
 }
--- DataAccess/Abstract/IOrderDal.cs
+++ DataAccess/Abstract/IOrderDal.cs
@@ -2,13 +2,11 @@
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
-    public interface ICategoryDal:IEntityRepository<Category>
+    public interface IOrderDal: IEntityRepository<Order>
     {
     }
 }
