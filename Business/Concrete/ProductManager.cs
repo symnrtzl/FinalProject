c91016571a73dcb7531eb5466acdcf775210e42f
symnrtzl <symnrtzl99@gmail.com>
2023-10-30 01:19:20 +0300

    JWT-1

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -29,6 +29,8 @@ namespace Business.Concrete
             _categoryService = categoryService;
         }
 
+        //Claim --> mesela admin
+        //[SecuredOpration("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
