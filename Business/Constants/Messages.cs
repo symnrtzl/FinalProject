0fc90cd1f846424f6e93f4a8141edcd603cba94b
symnrtzl <symnrtzl99@gmail.com>
2023-10-31 23:56:54 +0300

    JWT-3

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -11,10 +11,24 @@ namespace Business.Constants
     {
         public static string ProductAddedd = "Ürün Eklendi";
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
-        public static string MaintenanceTime = "Sistem Bakımda";
         public static string ProductsListed = "Ürünler Listelendi";
         public static string ProductCountOfCategoryError="Bir kategoride en fazla 10 ürün olabilir";
         public static string ProductNameAlreadyExists="Bu isimde zaten başka bir ürün var";
+
+
         public static string CategoryLimitExceded="Kategori limiti aşıldığı için yeni ürün eklenemiyor";
+
+
+        public static string MaintenanceTime = "Sistem Bakımda";
+        public static string AuthorizationDenied = "Yetkiniz yok";
+        public static string UserRegistered = "Kullanıcı başarıyla kaydedildi";
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Şifre hatalı";
+        public static string SuccessfulLogin = "Sisteme giriş başarılı";
+        public static string UserAlreadyExists = "Bu kullanıcı zaten mevcut";
+        public static string AccessTokenCreated="Access token başarıyla oluşturuldu";
+
+
+
     }
 }
