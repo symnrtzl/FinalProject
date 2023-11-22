a1d0354eb4da8da0f8c94917a7cffa5d00f1c3e1
symnrtzl <symnrtzl99@gmail.com>
2023-11-22 23:57:36 +0300

    Cache

---
+++ Core/CrossCuttingConcerns/Caching/ICacheManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Caching
+{
+    public interface ICacheManager
+    {
+        T Get<T>(string key);
+        object Get(string key);
+        void Add(string key, object value, int duration);
+        bool IsAdd(string key); // cache'de var mı? varsa cache'den getir, yoksa veritabanından getir
+        void Remove(string key); //cache'den atma
+        void RemoveByPattern(string pattern);
+
+    }
+}
